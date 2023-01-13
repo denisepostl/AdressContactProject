7867614528f928d3af482dba414dfcda90f0537a
postl <postl@LAPTOP-2A544SQ1>
2023-01-13 16:18:08 +0100

    calculate_id_for_tables

---
+++ adress/calculate_id.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+class CalculateID:
+
+    def __init__(self):
+        self.connection = sqlite3.connect("contacts.db")
+    
+    def calculate_phone_id(self): 
+        """This function calculates the phone_id that the user can add contacts comfortable."""
+        cur = self.connection.cursor()
+        id = """
+            SELECT 
+                Max(ID)
+            from PhoneNumber
+        """
+        cur.execute(id)
+        ID = cur.fetchall()
+        tup = ID[0]
+        self.New_ID = int(tup[0]) + int(1)
+        self.connection.commit()
+
+    def calculate_contact_id(self): 
+        """This function calculates the contact_id that the user can add contacts comfortable."""
+        cur = self.connection.cursor()
+        id = """
+            SELECT 
+                Max(ID)
+            from Contact
+        """
+        cur.execute(id)
+        ID = cur.fetchall()
+        tup = ID[0]
+        self.My_ID = int(tup[0]) + int(1)
+        self.connection.commit()
+
+    def calculate_adress_id(self): 
+        """This function calculates the adress_id that the user can add contacts comfortable."""
+        cur = self.connection.cursor()
+        id = """
+            SELECT 
+                Max(ID)
+            from Adress
+        """
+        cur.execute(id)
+        ID = cur.fetchall()
+        tup = ID[0]
+        self.New_ID = int(tup[0]) + int(1)
+        self.connection.commit()

5ce3c2373e4a420a5319685a60fb1f4afd6b407e
postl <postl@LAPTOP-2A544SQ1>
2023-01-19 14:22:37 +0100

    adress_category_datamodel

---
+++ database/adress_tables_category.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Contact(
+    ID INT PRIMARY KEY,
+    First_Name VARCHAR,
+    LastName VARCHAR,
+    Kategorie_ID INTEGER,
+    FOREIGN KEY(Kategorie_ID) REFERENCES Kategorie(ID)
+);
+
+CREATE TABLE Adress(
+    ID INT PRIMARY KEY,
+    Street VARCHAR,
+    PostCode INT,
+    City VARCHAR,
+    HouseNumber INT,
+    Contact_ID INT,
+    FOREIGN KEY(Contact_ID) REFERENCES Contact(ID)
+
+);
+
+CREATE TABLE PhoneNumber(
+    ID INT PRIMARY KEY,
+    PhoneNumber VARCHAR,
+    Contact_ID INT,
+    FOREIGN KEY(Contact_ID) REFERENCES Contact(ID)
+
+);
+
+CREATE TABLE Kategorie(
+    ID INT PRIMARY KEY,
+    Kategorie VARCHAR
+);

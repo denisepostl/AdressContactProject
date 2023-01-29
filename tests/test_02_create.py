eecc8b29cfa8f9a923ef0805216f5d31e3848eb7
postl <postl@LAPTOP-2A544SQ1>
2023-01-29 14:39:50 +0100

    update_test

--- tests/test_02_create.py
+++ tests/test_02_create.py
@@ -25,3 +25,31 @@ def test_if_tables_exists():
     ret = bool(c.fetchone())
     conn.close()
     return ret
+
+def test_if_tables_exists_category():
+    """Test if the Tables exist in adress.db"""
+
+    conn = sqlite3.connect('database/adress_cat.db')
+    c = conn.cursor()
+    c.execute("""
+        SELECT ID, First_Name, LastName
+        FROM Contact;
+    """, )
+
+    c.execute("""
+        SELECT ID, Street, PostCode, City, HouseNumber, Contact_ID
+        FROM Adress;
+    """, )
+
+    c.execute("""
+        SELECT ID, PhoneNumber, Contact_ID
+        FROM PhoneNumber;
+    """, )
+
+    c.execute("""
+        SELECT ID, Kategorie, Contact_ID
+        FROM Kategorie;
+    """, )
+    ret = bool(c.fetchone())
+    conn.close()
+    return ret

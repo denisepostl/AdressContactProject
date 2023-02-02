1f9bb720ae5e6f3dab1fa692a8783e0a840e8ebf
postl <postl@LAPTOP-2A544SQ1>
2023-02-02 19:01:52 +0100

    update_test

--- adress/update_for_gui.py
+++ adress/update_for_gui.py
@@ -6,42 +6,63 @@ class Updating():
         self.connection = sqlite3.connect("database/adress_cat.db")
 
 
-    def update_FName(self, new_name, old_name):
+    def get_id(self, first_name, last_name):
+        """This method is used to return the id from record when the user wants to update a record."""
         cur = self.connection.cursor()
-        cur.execute("UPDATE Contact SET First_Name = ? WHERE First_Name = ?" , (new_name, old_name))
+        get_id = """
+            SELECT
+	            a.ID
+            from Contact a
+            where a.First_Name like "%s"
+            and a.LastName like "%s";
+        """ %(first_name, last_name)
+
+        cur.execute(get_id)
+        
+        ID = cur.fetchall()
+        tup = ID[0]
+        self.My_ID = int(tup[0])
         self.connection.commit()
 
-    def update_LName(self, newLName, oldLName):
+        return self.My_ID
+        
+
+    def update_FName(self, new_name, id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE Contact SET LastName = ? WHERE LastName = ?" , (newLName, oldLName))
+        cur.execute("UPDATE Contact SET First_Name = ? WHERE ID = ?" , (new_name, id))
         self.connection.commit()
 
-    def update_PostCode(self, NewPLZ, OldPLZ):
+    def update_LName(self, newLName, id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE Adress SET PostCode = ? WHERE PostCode = ?" , (NewPLZ, OldPLZ))
+        cur.execute("UPDATE Contact SET LastName = ? WHERE ID = ?" , (newLName, id))
+        self.connection.commit()
+
+    def update_PostCode(self, NewPLZ, contact_id):
+        cur = self.connection.cursor()
+        cur.execute("UPDATE Adress SET PostCode = ? WHERE Contact_ID = ?" , (NewPLZ, contact_id))
         self.connection.commit()
     
-    def update_City(self, NewCity, OldCity):
+    def update_City(self, NewCity, contact_id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE Adress SET City = ? WHERE City = ?" , (NewCity, OldCity))
+        cur.execute("UPDATE Adress SET City = ? WHERE Contact_ID = ?" , (NewCity, contact_id))
         self.connection.commit()
 
-    def update_Street(self, NewStreet, OldStreet):
+    def update_Street(self, NewStreet, contact_id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE Adress SET Street = ? WHERE Street = ?" , (NewStreet, OldStreet))
+        cur.execute("UPDATE Adress SET Street = ? WHERE Contact_ID = ?" , (NewStreet, contact_id))
         self.connection.commit()
 
-    def update_HNR(self, newhnr, oldhnr):
+    def update_HNR(self, newhnr, contact_id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE Adress SET HouseNumber = ? WHERE HouseNumber = ?" , (newhnr, oldhnr))
+        cur.execute("UPDATE Adress SET HouseNumber = ? WHERE Contact_ID = ?"  , (newhnr, contact_id))
         self.connection.commit()
 
-    def update_Tel(self, newtel, oldtel):
+    def update_Tel(self, newtel, contact_id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE PhoneNumber SET PhoneNumber = ? WHERE PhoneNumber = ?" , (newtel, oldtel))
+        cur.execute("UPDATE PhoneNumber SET PhoneNumber = ? WHERE Contact_ID = ?"  , (newtel, contact_id))
         self.connection.commit()
 
-    def update_Category(self, Cat, OldCat):
+    def update_Category(self, Cat, contact_id):
         cur = self.connection.cursor()
-        cur.execute("UPDATE Kategorie SET Kategorie = ? WHERE Kategorie = ?" , (Cat, OldCat))
+        cur.execute("UPDATE Kategorie SET Kategorie = ? WHERE Contact_ID = ?" , (Cat, contact_id))
         self.connection.commit()
